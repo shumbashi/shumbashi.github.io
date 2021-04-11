df2e449b6ec5c4ebd8ab09ea08af61b5ac3a1e8b
Ahmed Shibani <a.shibani@email.ly>
2021-04-11 15:13:58 +0200

    Added a bunch of plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = 'Ahmed Shibani'
-SITENAME = 'Shumbashi Blog'
+SITENAME = "Shumbashi's Blog"
 SITEURL = ''
 
 PATH = 'content'
@@ -19,14 +19,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Libyan Spider', 'https://libyanspider.com/'),
+        )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/ashibani'),
+          ('GitHub', 'https://github.com/shumbashi'),
+          ('Twitter', 'https://twitter.com/ashibani'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -38,6 +37,33 @@ THEME = 'theme/pelican-bootstrap3'
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
 PLUGIN_PATHS = ['plugins'] 
-PLUGINS = ['i18n_subsites']
+PLUGINS = ['i18n_subsites', 'related_posts', 'series']
 BOOTSTRAP_THEME = 'flatly'
 OUTPUT_PATH = 'docs'
+DISPLAY_BREADCRUMBS = True
+# DISPLAY_ARTICLE_INFO_ON_INDEX = True
+# ABOUT_ME = "I'm Ahmed Shibani"
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+DISPLAY_ARCHIVE_ON_SIDEBAR = True
+# DISPLAY_AUTHORS_ON_SIDEBAR = True
+CC_LICENSE = "CC-BY"
+GITHUB_USER = "shumbashi"
+GITHUB_SKIP_FORK = True
+GITHUB_SHOW_USER_LINK = True
+TWITTER_CARDS = True
+TWITTER_USERNAME = "a.shibani"
+# SHARIFF = True
+ADDTHIS_PROFILE = "ra-6072ce1f837f9ede"
+SHOW_ARTICLE_AUTHOR = True
+SHOW_ARTICLE_CATEGORY = True
+PYGMENTS_STYLE = "solarizeddark"
+CUSTOM_CSS = 'static/css/custom.css'
+
+STATIC_PATHS = [
+  'images',
+  'extra'
+]
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/css/custom.css'},
+}
